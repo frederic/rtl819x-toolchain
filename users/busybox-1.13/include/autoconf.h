1ce7625f2691cfd444ccf6bb2f962dc9bbdcfbc9
user <user@user-laptop.(none)>
2013-06-29 03:01:08 +0200

    build config for RTL8196E_88E, with tcpdump & dropbear apps

--- users/busybox-1.13/include/autoconf.h
+++ users/busybox-1.13/include/autoconf.h
@@ -2,7 +2,7 @@
  * Automatically generated C config: don't edit
  * Busybox version: 1.13.4
  */
-#define AUTOCONF_TIMESTAMP "2013-01-04 17:00:40 CST"
+#define AUTOCONF_TIMESTAMP "2013-06-29 02:51:51 CEST"
 
 #define CONFIG_HAVE_DOT_CONFIG 1
 #define ENABLE_HAVE_DOT_CONFIG 1
